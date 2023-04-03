ef9a2b5de73617615b70e3b5b06af8ea4fae813b
mangogod0 <128722970+mangogod0@users.noreply.github.com>
2023-04-03 10:54:45 -0500

    Update Game.java

--- Mini flap/src/Game.java
+++ Mini flap/src/Game.java
@@ -1,36 +1,294 @@
 import javax.swing.*;
+
 import java.awt.*;
+
+import java.awt.image.BufferedImage;
+
 import java.awt.event.*;
 
-public class Main extends JFrame{
-	private static final int WIDTH =1800;
-	private static final int HEIGHT=1600;
-	
-	public Main () {
-		super("Mini Flap");
-		setSize(WIDTH, HEIGHT);
-		Game play = new Game();
-		
-		
-		
-		
-		setBackground(Color.GREEN);
-		  getContentPane().add(play);
-		
-			
-		setVisible(true);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		
-	}
-	
+ 
 
-	public static void main(String[] args) {
-		Main run = new Main();
-		
-		
+ 
 
-	}
+public class Game  extends JPanel implements Runnable, KeyListener{
 
+ 
+
+               
+
+                private BufferedImage back;
+
+                private int key, lives;
+
+         
+
+                private String guesses;
+
+               
+
+               
+
+                public Game() {
+
+                                new Thread(this).start();             
+
+                                this.addKeyListener(this);
+
+                                key =-1;
+
+                                lives=6;
+
+                         
+                                guesses = "";
+
+                               
+
+                }
+
+ 
+
+               
+
+               
+
+                public void run()
+
+                   {
+
+                                try
+
+                                {
+
+                                                while(true)
+
+                                                {
+
+                                                   Thread.currentThread().sleep(5);
+
+                            repaint();
+
+                         }
+
+                      }
+
+                                                catch(Exception e)
+
+                      {
+
+                      }
+
+                                }
+
+               
+
+ 
+
+               
+
+               
+
+               
+
+                public void paint(Graphics g){
+
+                               
+
+                                Graphics2D twoDgraph = (Graphics2D) g;
+
+                                if( back ==null)
+
+                                                back=(BufferedImage)( (createImage(getWidth(), getHeight())));
+
+                               
+
+ 
+
+                                Graphics g2d = back.createGraphics();
+
+               
+
+                               
+
+                                g2d.clearRect(0,0,getSize().width, getSize().height);
+
+                               
+
+                                g2d.setFont(new Font ("Times", Font.BOLD, 50));
+
+                             
+
+                               
+
+                                drawCircle(g2d);
+
+                                Color BLACK = new Color(0,0,0);
+
+                                g2d.setColor(BLACK);
+
+                                g2d.drawString(guesses,50, 600);
+
+                            
+
+                                g2d.drawString("Press Enter To Roll Another Word",100, 100);
+
+                               
+
+                               
+
+                               
+
+                                g2d.setFont(new Font ("Lora", Font.BOLD, 100));
+
+                               
+
+                             
+
+                               
+
+                                g2d.setColor(Color.white);
+
+                              
+                                twoDgraph.drawImage(back, null, 0, 0);
+
+ 
+
+
+
+                }
+
+               
+
+                public void drawCircle (Graphics g2d)
+
+                {                             
+
+                     
+
+                                }             
+
+                
+
+               
+
+                @Override
+
+                public void keyTyped(KeyEvent e) {
+
+                                // TODO Auto-generated method stub
+
+                               
+
+                }
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+                @Override
+
+                public void keyPressed(KeyEvent e) {
+
+                                // TODO Auto-generated method stub
+
+                               
+
+                                key= e.getKeyCode();
+
+                                System.out.println(key);
+
+                                char c= e.getKeyChar();
+
+                                System.out.println(Character.toUpperCase(c));
+
+                               
+
+                                      
+
+                                                guesses="";
+
+                                                lives=6;
+
+                               
+
+                                }
+
+                
+
+                public void guesses (char c) {
+
+                                boolean contains = true;
+
+                                for (int i=0; i<guesses.length(); i++) {
+
+                                                if (c==guesses.charAt(i)) {
+
+                                                                contains = false;
+
+                                                }
+
+                                }
+
+                                if(contains) {
+
+                                                guesses+= c + " ";
+
+                                                lives--;
+
+                                               
+
+                                }
+
+                }
+
+                                             
+
+ 
+
+                      
+
+               
+
+
+                                               
+
+                
+
+                               
+
+                
+
+ 
+
+ 
+
+ 
+
+                @Override
+
+                public void keyReleased(KeyEvent e) {
+
+               
+
+                               
+
+                               
+
+                }
+
+               
+
+               
+
+               
+
+ 
+
+               
 
 }
