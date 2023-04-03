e96670da51b07e672f52cb36e4434677e271c4ce
mangogod0 <128722970+mangogod0@users.noreply.github.com>
2023-04-03 10:55:22 -0500

    Update Main.java

--- Mini flap/src/Main.java
+++ Mini flap/src/Main.java
@@ -1,4 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class Main {
+public class Main extends JFrame{
+	private static final int WIDTH =1800;
+	private static final int HEIGHT=1600;
+	
+	public Main () {
+		super("Mini Flap");
+		setSize(WIDTH, HEIGHT);
+		Game play = new Game();
+		
+		
+		
+		
+		setBackground(Color.GREEN);
+		  getContentPane().add(play);
+		
+			
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+	}
+	
+
+	public static void main(String[] args) {
+		Main run = new Main();
+		
+		
+
+	}
+
 
 }
