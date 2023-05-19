2c4fd9ffd3243cf11a0d4043a9a59bc1788ba218
mangogod0 <128722970+mangogod0@users.noreply.github.com>
2023-05-19 10:31:02 -0500

    Update Brid.java

--- Mini flap/src/Brid.java
+++ Mini flap/src/Brid.java
@@ -1,33 +1,84 @@
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-
-import javax.swing.ImageIcon;
+import java.awt.Color;
+import java.awt.Graphics;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Background_image extends JFrame {
-	
-	JLabel L1;
-	
-	public Background_image() {
-		
-		setTitle("MiniFlap");
-		setSize(1200,1200);
-		setLocationRelativeTo(null);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
-		setLayout(new BorderLayout());
-		
-		setContentPane(new JLabel(new ImageIcon("C:\\Users\\s1890980\\OneDrive - Houston Independent School District\\Pictures\\Saved Pictures\\MiniFlap.png")));
-		setLayout(new FlowLayout());
-		L1 = new JLabel();
-		add(L1);
-		setSize(1200,700);
-	}
+public class Bird {
+    private int x;
+    private int y;
+    private int size;
+    
+    public Bird(int x, int y, int size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+    }
+    
+    public void update() {
+        // Add logic for bird movement, gravity, etc.
+    }
+    
+    public void jump() {
+        // Add logic for bird jump
+    }
+    
+    public void draw(Graphics g) {
+        g.setColor(Color.YELLOW);
+        g.fillOval(x, y, size, size);
 
-	public static void main (String arg[]) {
-		
-		new Background_image();
-	}
-	
+public class MiniFlap extends JPanel implements KeyListener {
+    private Bird bird;
+    
+    public MiniFlap() {
+        bird = new Bird(100, 200, 30);
+    }
+    
+    public void update() {
+        bird.update();
+        // Add logic for game updates (e.g., collision detection, score, etc.)
+    }
+    
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        bird.draw(g);
+        // Add drawing logic for other game components (e.g., pipes, background, etc.)
+    }
+    
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            bird.jump();
+        }
+    }
+    
+    public void keyReleased(KeyEvent e) {
+        // Handle key released events if necessary
+    }
+    
+    public void keyTyped(KeyEvent e) {
+        // Handle key typed events if necessary
+    }
+    
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Flappy Bird");
+        FlappyBirdGame game = new FlappyBirdGame();
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(game);
+        frame.addKeyListener(game);
+        frame.setSize(400, 600);
+        frame.setVisible(true);
+        
+        while (true) {
+            game.update();
+            game.repaint();
+            try {
+                Thread.sleep(10); // Adjust the delay to control the game's speed
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
